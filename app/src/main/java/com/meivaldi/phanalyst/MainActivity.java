a02ffd24b4f9a0afc4291480e9e8e2e10a01ecb9
andreepratama27 <andreepratama27@gmail.com>
2018-03-13 23:40:39 +0700

    Nothing

--- app/src/main/java/com/meivaldi/phanalyst/MainActivity.java
+++ app/src/main/java/com/meivaldi/phanalyst/MainActivity.java
@@ -27,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-
 
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
 
